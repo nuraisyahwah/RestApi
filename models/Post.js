7b484b7d1e57fba38158c7e9b6c7d42451e4623b
nuraisyah <nuraisyahwah19@gmail.com>
2023-06-13 22:43:59 +0700

    .

--- models/Post.js
+++ models/Post.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose')
 
 const postSchema = mongoose.Schema({
-    content: {
+    nama: {
         type: String,
-        require: true
+        required: true
     },
     user_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
+<<<<<<< Updated upstream
+=======
+    },
+    alamat: {
+        type: String,
+        required: true
+>>>>>>> Stashed changes
     },
     created_date: {
         type: Date,
@@ -17,13 +24,8 @@ const postSchema = mongoose.Schema({
     modified_date: {
         type: Date,
         default: null
-    },
-    // username: {
-    //     type: String,
-    //     require: true
-    // }
+    }
 }, {
     versionKey: false
 })
-
 module.exports = mongoose.model('Post', postSchema, 'post')
